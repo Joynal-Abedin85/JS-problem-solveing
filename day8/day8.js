675973a950d5c6080e00713ae604cc251a44d14d
kazi tareq <kazitareq321@gmail.com>
2025-05-16 22:02:09 +0600

    bear and big brother problem solve

---
+++ day8/day8.js
@@ -0,0 +1,24 @@
+
+// Read input from standard input
+// const fs = require('fs');
+// const input = fs.readFileSync(0, 'utf8').trim().split(' ');
+
+let a = 4
+let b = 7
+
+let years = 0;
+
+while (a <= b) {
+    a *= 3;
+    b *= 2;
+    years++;
+}
+
+
+console.log(years);
+
+    
+
+
+
+
