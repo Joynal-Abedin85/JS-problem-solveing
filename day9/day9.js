baa8366ae8fbf350e713955363c1fae3c98dacd8
kazi tareq <kazitareq321@gmail.com>
2025-05-19 23:54:11 +0600

    filtering some array number

--- day9/day9.js
+++ day9/day9.js
@@ -12,8 +12,16 @@ let c = "2,5,6,9"
 let arr = [3,4,5,6,7,8]
 
 let b = c.split(",").map((a,b) => a % b == 0)
-let arrmap = arr.map((a,b) => a - b )
+let arrmap = arr.map((a,b) => a-b )
 
 console.log(arrmap)
 console.log(b)
+
+
+let filt = [3,4,5,6,7,7,8,9]
+
+let filtering = filt.filter((a,b) => a % b == 0)
+
+console.log(filtering)
+
 
